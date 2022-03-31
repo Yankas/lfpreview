79553efd1fd3be8614f66a77dda6fbf28ad96f5f
Yankas <christopher.sbrisny@gmail.com>
2022-03-31 11:49:30 +0200

    Improved image handling, cleanup

--- lfpreview.ml
+++ lfpreview.ml
@@ -29,7 +29,7 @@ https://pkg.go.dev/github.com/gokcehan/lf#hdr-Configuration
 type position = {  width : string; height: string; left: string; top: string }
 type file_type = { 
     extensions : string list;
-    handler : string -> position -> (string, string) result
+    handler : string -> position -> (int, string) result
 }
 type file_match_result =
 | Matched of file_type
@@ -37,14 +37,13 @@ type file_match_result =
 | Fatal of string
 
 module Shell = struct
-
     (** [has_cmd cmd] uses the unix builtin "command" to check if the command [cmd] is available on the system. *)
     let has_cmd cmd = match Unix.system ("command " ^ cmd) with | WEXITED 0 -> true | _ -> false 
 
-    let run cmd : (string, string) result = 
+    let run cmd : (int, string) result = 
         let error fmt code = Error(Printf.sprintf fmt cmd (string_of_int code)) in
         match Unix.system cmd with
-        | WEXITED 0 -> Ok("")
+        | WEXITED 0 -> Ok(0)
         | WEXITED n -> error "Command `%s` failed \n program aborted with exit code: `%s`." n
         | WSIGNALED n -> error "Command `%s` failed \n program aborted by signal: `%s`." n
         | WSTOPPED n -> error "Command `%s` failed \n stopped by signal: `%s`." n
@@ -52,21 +51,16 @@ module Shell = struct
     (** [read_process_lines command] runs the [command] using #!/bin/sh until termination
         returning its standard output as a string list where each new line represents 
         one element of the list. *)
-    let run_read_lines prog : (string list, string) result  =
+    let run_read prog = 
         let lines = ref [] in
-            let in_channel = Unix.open_process_in prog in
+        let in_channel = Unix.open_process_in prog in
         begin
             try
                 while true do lines := input_line in_channel :: !lines done;
             with End_of_file -> ignore (Unix.close_process_in in_channel)
         end;    
-        
-        Ok(List.rev !lines)
-         
-    let run_read prog = 
-        match run_read_lines prog with 
-        | Ok str -> Ok(str |> String.concat "\n") 
-        | e -> Error("TODO: run_read prog failed")
+        (List.rev !lines)
+        |> String.concat "\n"
 
     (** [run_cmd fmt] runs the command specified by [fmt] using the same syntax as Prinft.sprintf
         example: run_cmd "ls %s %s" "-las" "/usr/bin" *)    
@@ -83,16 +77,12 @@ module Shell = struct
                 match run (Printf.sprintf "kitty +icat --silent --transfer-mode file --place %sx%s@%sx%s %s" pos.width pos.height pos.left pos.top filename) with
                 (*match run (Printf.sprintf "kitty +kitten icat --place %sx%s@%sx%s %s" pos.width pos.height pos.left pos.top filename) with *)
                 | Error _ -> Error(Printf.sprintf "Couldn't print image file `%s` to terminal" filename)
-                | s -> Ok("")
+                | s -> Ok(1)
             end
         | None | Some _ -> Error("Couldn't display image $TERM is not set")
          
-    let run_print cmd = 
-        match run_read cmd with
-        | Ok x -> print_string x; Ok("")
-        | Error x -> Error(x)
+    let run_print cmd = print_string (run_read cmd); Ok(0)
 end
-
 
 
 let file_types = 
@@ -195,16 +185,17 @@ let preview () =
     in
     begin
         match result with
-        | Matched file_type -> file_type.handler (Printf.sprintf "\"%s\"" file) print_position |> ignore; 1
+        | Matched file_type -> 
+            (match file_type.handler (Printf.sprintf "\"%s\"" file) print_position with
+            | Ok exit_code -> exit_code
+            | _ -> 0)
         | Unresolved -> print_string "No handler found for this file type."; 0
         | Fatal m -> print_string ("An error has occured while creating the preview: \n" ^ m); 1
     end
 
 let clean_up () = 
     match Unix.system "kitty +icat --clear --silent --transfer-mode file" with
-    | WEXITED code 
-    | WSIGNALED code 
-    | WSTOPPED code-> code
+    | WEXITED code | WSIGNALED code | WSTOPPED code-> code
 
 
 let () =
